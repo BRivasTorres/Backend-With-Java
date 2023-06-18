4ba89601812139730d14377bca7df14506037062
Billy Torres <btorresr75@gmail.com>
2023-06-18 10:25:30 -0600

    Condicionales en Java -- Condicional If/Else

---
+++ JavaGeneral/ConditionalsExamples.java
@@ -0,0 +1,10 @@
+public class ConditionalsExamples {
+    public static void main(String[] args) {
+        int age = 28;
+        if (age >= 18) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("No esta permitido");
+        }
+    }
+}
