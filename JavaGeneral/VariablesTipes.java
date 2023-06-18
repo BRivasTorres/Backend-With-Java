156f99c941be077cfb56b953a91700c387e798a9
Billy Torres <btorresr75@gmail.com>
2023-06-18 09:09:34 -0600

    Tipo de variable numerico en Java

---
+++ JavaGeneral/VariablesTipes.java
@@ -0,0 +1,8 @@
+public class VariablesTipes {
+    public static void main(String[] args) {
+        System.out.println("Tipos de variables");
+        // *Para valores enteros */
+        int age = 30;
+        System.out.println(age);
+    }
+}
