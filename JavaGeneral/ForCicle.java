010fef2f322908c9f926190869780bbe32029551
Billy Torres <btorresr75@gmail.com>
2023-06-18 10:38:21 -0600

    Condicionales en Java -- Condicional For

---
+++ JavaGeneral/ForCicle.java
@@ -0,0 +1,7 @@
+public class ForCicle {
+    public static void main(String[] args) {
+        for (int count = 0; count <= 10; count += 2) {
+            System.out.println(count);
+        }
+    }
+}
