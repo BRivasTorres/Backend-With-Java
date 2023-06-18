9bc972ae5637d98551306e7631255822e9cc411a
Billy Torres <btorresr75@gmail.com>
2023-06-18 09:20:55 -0600

    Cast, tipos de conversion numericos y bytes

--- JavaGeneral/VariablesTipes.java
+++ JavaGeneral/VariablesTipes.java
@@ -4,5 +4,17 @@ public class VariablesTipes {
         // *Para valores enteros */
         int age = 30;
         System.out.println(age);
+        // *Para valores decimales */
+        double salary = 1250.50;
+        System.out.println(salary);
+
+        // *Este proceso de convertir un dato a otro se le llama Cast */
+        double var1 = 2309.38;
+        System.out.println(var1);
+        int var1Enter = (int) var1;
+        System.out.println(var1Enter);
+
+        long test = 484824323L;
+        System.out.println(test);
     }
 }
