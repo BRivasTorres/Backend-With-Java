6acbe67c53e2851c6898ac07f86d78efc66c434d
Billy Torres <btorresr75@gmail.com>
2023-06-19 11:58:17 -0600

    Referencias en Java

--- OOP Java/Bytebanc/CrearCuenta.java
+++ OOP Java/Bytebanc/CrearCuenta.java
@@ -3,5 +3,9 @@ public class CrearCuenta {
         Cuenta primeraCuenta = new Cuenta();
         primeraCuenta.saldo = 1000;
         System.out.println(primeraCuenta.saldo);
+
+        Cuenta segundaCuenta = new Cuenta();
+        segundaCuenta.saldo = 500;
+        System.out.println(segundaCuenta.saldo);
     }
 }
