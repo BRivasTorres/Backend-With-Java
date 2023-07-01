1367b999509908adf6ec946d9f8a6bb2af09a1d2
Billy Torres <btorresr75@gmail.com>
2023-07-01 17:24:43 -0600

    Herencia por medio de la palabra extends en Java

--- Java Herencia e Interfaces/Gerente.java
+++ Java Herencia e Interfaces/Gerente.java
@@ -1,3 +1,11 @@
-public class Gerente {
+public class Gerente extends Funcionario {
+    private String clave;
 
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public boolean iniciarSecion(String clave) {
+        return clave == "HolaMundo";
+    }
 }
---
+++ Java Herencia e Interfaces/TestGerente.java
@@ -0,0 +1,7 @@
+public class TestGerente {
+    public static void main(String[] args) {
+        Funcionario gerente = new Funcionario();
+        gerente.setSalario(5000);
+        System.out.println(gerente.getSalario());
+    }
+}
