8969bc48cd3053888016396988f924992f826305
Billy Torres <btorresr75@gmail.com>
2023-07-18 17:39:54 -0600

    Conociendo Arrays en Java

---
+++ Java y Java.util/TestMain.java
@@ -0,0 +1,12 @@
+public class TestMain {
+    public static void main(String[] args) {
+        int edad = 20;
+        int edad2 = 15;
+        int edad3 = 67;
+
+        int[] edades = new int[5];
+        edades[2] = 30;
+
+        System.out.println(edades[2]);
+    }
+}
