3a577e958b67518012785397c4fa0c425dad5f06
Billy Torres <btorresr75@gmail.com>
2023-07-21 17:22:27 -0600

    Genericos en Java

--- Java y Java.util/TestArrayList.java
+++ Java y Java.util/TestArrayList.java
@@ -2,13 +2,17 @@ import java.util.ArrayList;
 
 public class TestArrayList {
     public static void main(String[] args) {
-        ArrayList lista = new ArrayList();
+        ArrayList<Cuenta> lista = new ArrayList();
         Cuenta cc = new CuentaCorriente(11, 22);
         Cuenta cc2 = new CuentaCorriente(13, 42);
 
         lista.add(0, cc2);
 
-        Cuenta obtenerCuenta = (Cuenta) lista.get(0);
+        Cuenta obtenerCuenta = lista.get(0);
         System.out.println(obtenerCuenta);
+
+        for (Cuenta cuenta : lista) {
+            System.out.println(cuenta);
+        }
     }
 }
