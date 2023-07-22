9a9ca40c99aad8b6caf77c7fa182a736c3edf596
Billy Torres <btorresr75@gmail.com>
2023-07-22 16:47:25 -0600

    Wrappers en Java

---
+++ Java y Java.util/TestWrappers.java
@@ -0,0 +1,8 @@
+public class TestWrappers {
+    public static void main(String[] args) {
+        Double numerDouble = 22.9;
+        Boolean verdarero = true;
+
+        Double numDouble2 = Double.valueOf(33);
+    }
+}
